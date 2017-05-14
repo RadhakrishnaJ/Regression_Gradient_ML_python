de15f82635b047e0bf0512b30c1c60c2d46e2db6
RadhakrishnaJ <jellaradhakrishna@gmail.com>
2017-05-14 12:29:32 +0530

    Add files via upload

---
+++ Regression_Gradient_Descent/Simple_regression.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+""" @author - Radhakrishna Jella
+
+Simple linear regression with one feature is explained here. 
+By chnaging input file, adding new features it can be extended 
+for multiple variables. 
+
+"""
+
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+
+
+training_data = pd.read_csv("Input_data.txt")
+
+print(training_data.describe())
+print(training_data.head())
+
+model_input = training_data['x']  
+"""use list of fetaure names for multiple features"""
+model_target = training_data['y']
+
+
+Regr_model = LinearRegression()
+print("fit")
+Regr_model.fit(model_input.reshape(-1,1), model_target)
+""" Reshape is not needed is multiple features exists"""
+
+print("Model Coefficients")
+print("intercept", Regr_model.intercept_)
+print("coefficient", Regr_model.coef_)
+
+predicted_y = Regr_model.predict(model_input.reshape(-1,1))
+print("metrics - r2")
+print(metrics.r2_score(model_target, predicted_y))
+print("Program completed successfully")
